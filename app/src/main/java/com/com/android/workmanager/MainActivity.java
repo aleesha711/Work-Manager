53853803dc1d1a4778cf6c7079c4b67fbe80c2cc
Aleesha <aleesha_kanwal@yahoo.com>
2018-12-08 00:37:02 +0500

    adding files

---
+++ app/src/main/java/com/com/android/workmanager/MainActivity.java
@@ -0,0 +1,96 @@
+package com.com.android.workmanager;
+
+import android.arch.lifecycle.Observer;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+
+import androidx.work.Constraints;
+import androidx.work.Data;
+import androidx.work.OneTimeWorkRequest;
+import androidx.work.PeriodicWorkRequest;
+import androidx.work.WorkManager;
+import androidx.work.WorkStatus;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextView = findViewById(R.id.textView);
+
+        Data data = new Data.Builder()
+                .putString(MyWorker.EXTRA_TITLE, "Message from Activity!")
+                .putString(MyWorker.EXTRA_TEXT, "Hi! I have come from activity.")
+                .build();
+
+        Constraints constraints = new Constraints.Builder()
+                .setRequiresCharging(true)
+                .build();
+
+        final OneTimeWorkRequest simpleRequest = new OneTimeWorkRequest.Builder(MyWorker.class)
+                .setInputData(data)
+                .setConstraints(constraints)
+                .addTag("simple_work")
+                .build();
+
+        final PeriodicWorkRequest periodicWorkRequest = new PeriodicWorkRequest.Builder(MyWorker.class, 12, TimeUnit.HOURS)
+                .addTag("periodic_work")
+                .build();
+
+        final UUID workId = simpleRequest.getId();
+
+        findViewById(R.id.simpleWorkButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                WorkManager.getInstance().enqueue(simpleRequest);
+            }
+        });
+
+        findViewById(R.id.cancelWorkButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //WorkManager.getInstance().cancelAllWorkByTag("simple_work");
+                WorkManager.getInstance().cancelWorkById(workId);
+            }
+        });
+
+        findViewById(R.id.periodicWorkButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                WorkManager.getInstance().enqueue(periodicWorkRequest);
+            }
+        });
+
+        findViewById(R.id.cancelPeriodicWorkButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                WorkManager.getInstance().cancelWorkById(periodicWorkRequest.getId());
+            }
+        });
+
+        WorkManager.getInstance().getStatusById(simpleRequest.getId())
+                .observe(this, new Observer<WorkStatus>() {
+                    @Override
+                    public void onChanged(@Nullable WorkStatus workStatus) {
+                        if (workStatus != null) {
+                            mTextView.append("SimpleWorkRequest: " + workStatus.getState().name() + "\n");
+                        }
+
+                        if (workStatus != null && workStatus.getState().isFinished()) {
+                            String message = workStatus.getOutputData().getString(MyWorker.EXTRA_OUTPUT_MESSAGE, "Default message");
+                            mTextView.append("SimpleWorkRequest (Data): " + message);
+                        }
+                    }
+                });
+    }
+}
